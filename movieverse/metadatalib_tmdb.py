2114f1f2fc6ba4acd2732238c2b3e356089ee075
KNW <koen.baak@mn.nl>
2021-03-09 16:43:34 +0100

    skeleton built

---
+++ movieverse/metadatalib_tmdb.py
@@ -0,0 +1,9 @@
+# ------ Python standard library imports ---------------------------------------
+# ------ External imports ------------------------------------------------------
+# ------ Imports from own package or module ------------------------------------
+from movieverse.metadatalib_fallback_base import MetaDataLibWithFallback
+#-------------------------------------------------------------------------------
+
+
+class TMDBMetaDataLib(MetaDataLibWithFallback):
+    pass
