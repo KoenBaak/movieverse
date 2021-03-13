d867baec095acbec2380ac41b44babf5b0651c9a
KNW <koen.baak@mn.nl>
2021-03-13 20:54:50 +0100

    delete bad metadata system

--- movieverse/metadatalib_tmdb.py
+++
@@ -1,9 +0,0 @@
-# ------ Python standard library imports ---------------------------------------
-# ------ External imports ------------------------------------------------------
-# ------ Imports from own package or module ------------------------------------
-from movieverse.metadatalib_fallback_base import MetaDataLibWithFallback
-#-------------------------------------------------------------------------------
-
-
-class TMDBMetaDataLib(MetaDataLibWithFallback):
-    pass
